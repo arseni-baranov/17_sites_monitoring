e1eb0ea6aa7b812185318e9f3c55341132b4c362
coding@justlike.pro <coding@justlike.pro>
2016-12-18 04:58:48 +0500

    ''

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,63 @@
+import requests
+import random
+import urllib
+
+
+def get_proxies_list():
+    proxy_list = "http://www.freeproxy-list.ru/api/proxy?anonymity=false&token=demo"
+    html = requests.get(proxy_list).text
+    proxies = html.split('\n')
+    return proxies
+
+
 def load_urls4check(path):
-    pass
+    return [line.rstrip('\n') for line in open(path)]
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        return requests.get(url).status_code
+    except requests.RequestException:
+        print('It seems that', url, 'is down')
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def get_domain_expiration_date(url, proxies_list):
+    url = urllib.parse.urlparse(url).netloc
+    whois_api = "https://madchecker.com/domain/api/{}?properties=expiration"
+    proxy = {"http": random.choice(proxies_list)}
+
+    while 1:
+        try:
+            document = requests.get(whois_api.format(url), proxies=proxy).json()
+            expiration_date = document['data']['expiration']
+        except (requests.exceptions.ProxyError, KeyError):
+            return 'Unknown'
+        else:
+            return expiration_date
+
+
+def pretty_print(domains_data):
+    print(''.center(60, '='), end='\n')
+    print('URL STATUS CHECKER'.center(60, ' '), end='\n')
+    print(''.center(60, '='), end='\n\n')
+    for domain in domains_data:
+        print('Url: {}\nStatus: {}\nExpires on: {}'.format(domain[0], domain[1], domain[2]), end='\n\n')
+
+
+def main():
+    domains_data = []
+
+    urls = load_urls4check('urls')
+    proxies = get_proxies_list()
+
+    for index, url in enumerate(urls, 1):
+        print('fetching url', index, '...')
+        status = is_server_respond_with_200(url)
+        if status:
+            expiration_date = get_domain_expiration_date(url, proxies)
+            domains_data.extend([(url, status, expiration_date)])
+
+    pretty_print(domains_data)
 
 if __name__ == '__main__':
-    pass
+    main()
