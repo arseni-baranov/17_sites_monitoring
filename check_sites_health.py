524e4cc535cbc095c29e757ced9f3b67a9fc39ea
arseni-baranov <arseni-baranov@yandex.ru>
2016-12-23 22:22:51 +0500

    Внесёт важный рефакторинг
    В get_proxies_list() и get_domain_expiration_date() заменит get-параметры в url строке и передаст их через requests.get
    Подправит is_server_respond_with_200(), теперь функция только возвращает состояние сервера
    Подправит pretty_print, теперь Response это http-код отданный сервером, и в начале появится Status - это итог запроса, который выведет 'Ok' / 'Not ok'
    Дополнит main(), условием на существование даты истечения домена, что даст конечный результат переменной url_status - Ok или Not Ok.
    Дополнит main условием на существования response (ответа от сервера вообще), и в противном случае даст отрицательные сообщения на expiration_date, response и url_status

--- check_sites_health.py
+++ check_sites_health.py
@@ -4,34 +4,35 @@ import urllib
 
 
 def get_proxies_list():
-    proxy_list = "http://www.freeproxy-list.ru/api/proxy?anonymity=false&token=demo"
-    html = requests.get(proxy_list).text
+    proxy_list = "http://www.freeproxy-list.ru/api/proxy"
+    html = requests.get(proxy_list, params={'anonymity': 'false', 'token': 'demo'}).text
     proxies = html.split('\n')
     return proxies
 
 
 def load_urls4check(path):
-    return [line.rstrip('\n') for line in open(path)]
+    with open(path) as urls_file:
+        return [line.rstrip('\n') for line in urls_file]
 
 
 def is_server_respond_with_200(url):
     try:
         return requests.get(url).status_code
     except requests.RequestException:
-        print('It seems that', url, 'is down')
+        return
 
 
 def get_domain_expiration_date(url):
     url = urllib.parse.urlparse(url).netloc
-    whois_api = "https://madchecker.com/domain/api/{}?properties=expiration"
+    whois_api = "https://madchecker.com/domain/api/{}".format(url)
     proxy = {"http": random.choice(get_proxies_list())}
 
     while 1:
         try:
-            document = requests.get(whois_api.format(url), proxies=proxy).json()
+            document = requests.get(whois_api, params={'properties': 'expiration'}, proxies=proxy).json()
             expiration_date = document['data']['expiration']
         except (requests.exceptions.ProxyError, KeyError):
-            return 'Unknown'
+            return 0
         else:
             return expiration_date
 
@@ -41,19 +42,29 @@ def pretty_print(domains_data):
     print('URL STATUS CHECKER'.center(60, ' '), end='\n')
     print(''.center(60, '='), end='\n\n')
     for domain in domains_data:
-        print('Url: {}\nStatus: {}\nExpires on: {}'.format(domain[0], domain[1], domain[2]), end='\n\n')
+        print('{}\nStatus: {}\nResponse: {}, Expires On: {}'.format
+              (domain[0], domain[3], domain[1], domain[2]), end='\n\n')
 
 
 def main():
     domains_data = []
+
     urls = load_urls4check(input('Enter the path to a textfile with urls: '))
 
     for index, url in enumerate(urls, 1):
         print('fetching url', index, '...')
-        status = is_server_respond_with_200(url)
-        if status:
+        response = is_server_respond_with_200(url)
+
+        if response:
             expiration_date = get_domain_expiration_date(url)
-            domains_data.extend([(url, status, expiration_date)])
+            if expiration_date:
+                url_status = 'Ok'
+            else:
+                url_status = 'Not ok'
+        else:
+            expiration_date, response, url_status = 'n/a', 'n/a', 'Not Ok'
+
+        domains_data.extend([(url, response, expiration_date, url_status)])
 
     pretty_print(domains_data)
 
